5ec4d8d6595c3980ce541934c3782138c3e3a5c3
NikitaMordikQS <nmordik@qualium-systems.com>
2021-11-25 13:37:29 +0200

    second test push 25/11

--- Build/buildWEBXR.framework.js
+++ Build/buildWEBXR.framework.js
@@ -2411,7 +2411,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 345e4;
+STATICTOP = STATIC_BASE + 3450064;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -4465,7 +4465,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 345e4;
+var STATIC_BUMP = 3450064;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
